415dd17cf0c61b59d1dc8f584a46751ca338c430
20214244-ship-it <20214244@s.scnu.ac.kr>
2026-05-13 15:39:35 +0900

    7장

---
+++ CH07/Assets/2.Script/TargetController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    GameObject player;
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    private void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Bamsongi"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
